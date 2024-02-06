5f6eb34295be8aee98b2262195304f928ce74797
Andrii Nasinnyk <anasinnyk@pm.me>
2024-02-06 18:14:13 +0200

    gtk

--- nixos/home-manager/hyprland/default.nix
+++ nixos/home-manager/hyprland/default.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
+let
+  gruvbox = import ./gruvbox-plus.nix { inherit pkgs; };
+in
 {
   home.packages = with pkgs; [
     nerdfonts
@@ -12,10 +15,12 @@
     wofi
     wl-clipboard
     polkit_gnome
+    libsForQt5.polkit-kde-agent
     hyprland-protocols
     hyprlang
     libdrm
     pipewire
+    wireplunmber
     sdbus-cpp
     wayland-protocols
     xdg-desktop-portal-hyprland
@@ -25,7 +30,24 @@
     brillo
     watchexec
     pavucontrol
+    qt5-wayland
+    qt6-wayland
+    mako
+    swaync
+    bibata-cursors
+    adw-gtk3
   ];
+  
+  gtk.enable = true;
+  gtk.cursorTheme.package = pkgs.bibata-cursors;
+  home.file = {
+    ".icons/bibata".source = "${pkgs.bibata-cursors}/share/icons/Bibata-Modern-Classic";
+  };
+  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3";
+  gtk.iconTheme.package = gruvbox;
+  gtk.iconTheme.name = "GruvboxPlus";
 
   xdg.enable = true;
 
